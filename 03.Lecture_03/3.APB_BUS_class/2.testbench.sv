69eb5fd1bd933fa2872c1478a7164ad82d92d460
Mario Todorov <mario.yankov.todorov@gmail.com>
2021-05-25 17:54:53 +0300

    Add files

---
+++ 03.Lecture_03/3.APB_BUS_class/2.testbench.sv
@@ -0,0 +1,15 @@
+`include "rand_class.sv"
+
+module rand_top();
+  
+  initial begin
+  	rand_class rand_signals = new();
+    
+    repeat (5) begin
+      rand_signals.randomize(); 
+      $display("PADDR = %d PWRITE = %b PSEL = %b PWDATA = %b PREADY = %b", rand_signals.PADDR, rand_signals.PWRITE, rand_signals.PSEL, rand_signals.PWDATA, rand_signals.PREADY);
+    end
+    
+  end
+
+endmod
---
+++ 03.Lecture_03/6.Mailboxes/1.mailboxes.sv
@@ -0,0 +1,21 @@
+// To make errors less likely, we use parameterized mailboxes
+typedef mailbox #(string) str_mailbox;
+
+module mailbox_test();
+  
+  string str;
+  
+  str_mailbox smbx = new();
+  
+  initial begin
+    //smbx.put(1); returns error
+    smbx.put("Test_word");
+  end
+  
+  initial begin
+    smbx.get(str);
+    $display(str);
+  end
+  
+  
+endmodule
